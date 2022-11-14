9d8ce60b397280359e90d7b4ff5d61e08b3b917d
Mr.Yogesh <yogeshvanzara98@gmail.com>
2022-11-15 01:57:09 +0530

    home start

--- src/page/home/Home.jsx
+++ src/page/home/Home.jsx
@@ -1,6 +1,15 @@
 import React from "react";
-import { Box, Button, Typography } from "@mui/material";
+import {
+  CardContent,
+  Box,
+  Button,
+  Card,
+  CardMedia,
+  Typography,
+  Grid,
+} from "@mui/material";
 import hero from "../../images/home.jpg";
+import home1 from "../../images/home1.jpg";
 
 const Home = () => {
   return (
@@ -13,7 +22,7 @@ const Home = () => {
           backgroundPositionX: "center",
           backgroundPositionY: "center",
           backgroundSize: "cover",
-          height: { xs: "400px", sm: "600px" },
+          height: { xs: "400px", sm: "500px" },
           width: "100%",
         }}
       >
@@ -78,6 +87,45 @@ const Home = () => {
             </Button>
           </Box>
         </Box>
+      </Box>
+      <Box maxWidth={"95vw"} width="100%" margin={"2rem auto"}>
+        <Card>
+          <Grid container spacing={2}>
+            <Grid item xs={4} margin={"0 auto"}>
+              <CardMedia component="img" image={home1} alt="green iguana" />
+            </Grid>
+            <Grid item xs={12} md={8}>
+              <Typography
+                variant="h5"
+                component="div"
+                textAlign={"center"}
+                mt="1rem"
+              >
+                Get Online Job Support
+              </Typography>
+              <Typography
+                mt="1rem"
+                variant="h5"
+                component="div"
+                textAlign={"center"}
+              >
+                From Real Time Professional
+              </Typography>
+              <CardContent>
+                <Typography variant="body2" color="text.primary">
+                  If you are a fresher or experienced candidate who is new to a
+                  technology or struggling to complete work alone, we have a
+                  team of experienced professionals who can help you to complete
+                  assigned task in time and also train you to handle future
+                  requirement on your own. Based in India, we provide proxy
+                  online job support through net meeting, skype or team viewer
+                  from India in the USA, UK, Canada, Australia, and other
+                  countries at quite affordable prices.
+                </Typography>
+              </CardContent>
+            </Grid>
+          </Grid>
+        </Card>
       </Box>
     </Box>
   );
