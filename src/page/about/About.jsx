4e62107df300574f31e6c418807e305116784392
Mr.Yogesh <yogeshvanzara98@gmail.com>
2022-11-16 02:35:55 +0530

    contact start

--- src/page/about/About.jsx
+++ src/page/about/About.jsx
@@ -1,7 +1,24 @@
 import React from "react";
+import { Box } from "@mui/system";
+import about from "../../images/about.jpg";
 
 const About = () => {
-  return <div>About</div>;
+  return (
+    <>
+      <Box
+        sx={{
+          backgroundImage: `url(${about})`,
+          backgroundRepeat: "no-repeat",
+          backgroundColor: "black",
+          backgroundPositionX: "center",
+          backgroundPositionY: "center",
+          backgroundSize: "cover",
+          height: { xs: "380px", sm: "560px", md: "430px" },
+          width: "100%",
+        }}
+      ></Box>
+    </>
+  );
 };
 
 export default About;
