7dd9d33278ad6506872a9ad5cad327c11e0d9320
Mr.Yogesh <yogeshvanzara98@gmail.com>
2022-11-16 00:05:50 +0530

    contact start

--- src/page/contact/Contact.jsx
+++ src/page/contact/Contact.jsx
@@ -4,13 +4,33 @@ import {
   Button,
   Card,
   Container,
+  FormControl,
+  FormHelperText,
   Stack,
   TextField,
   Typography,
 } from "@mui/material";
 import contact from "../../images/contact.webp";
+import { useForm, ValidationError } from "@formspree/react";
+import { Link } from "react-router-dom";
 
 const Contact = () => {
+  const [state, handleSubmit] = useForm("maykaoqj");
+  if (state.succeeded) {
+    return (
+      <section className="error-page section">
+        <div className="error-container">
+          <h1>Thanks</h1>
+          <p>The Form submitted successfully</p>
+          <div className="error">
+            <Link to="/" className="btn btn-primary">
+              back home
+            </Link>
+          </div>
+        </div>
+      </section>
+    );
+  }
   return (
     <>
       <Box
@@ -33,46 +53,89 @@ const Contact = () => {
       />
       <Container>
         <Card sx={{ mt: "2rem", mb: "2rem", padding: "2rem" }}>
-          <Stack spacing={2}>
-            <Typography variant="h6" textAlign={"center"}>
-              GET IN TOUCH
-            </Typography>
-            <TextField
-              id="outlined-basic"
-              label="Name"
-              variant="outlined"
-              sx={{ width: "100%" }}
-            />
-            <TextField
-              id="outlined-basic"
-              label="Mail"
-              variant="outlined"
-              sx={{ width: "100%" }}
-            />
-            <TextField
-              id="outlined-basic"
-              label="Mobile"
-              variant="outlined"
-              sx={{ width: "100%" }}
-            />
-            <TextField
-              id="outlined-multiline-static"
-              label="Message"
-              multiline
-              rows={4}
-              sx={{ width: "100%" }}
-            />
-            <Button
-              variant="contained"
-              sx={{
-                maxWidth: "1rem",
-                margin: "0 auto",
-                padding: "0.5rem 4rem",
-              }}
-            >
-              Submit
-            </Button>
-          </Stack>
+          <Typography variant="h6" textAlign={"center"}>
+            GET IN TOUCH
+          </Typography>
+          <form
+            action="https://formspree.io/f/maykaoqj"
+            method="POST"
+            className="form"
+            onSubmit={handleSubmit}
+          >
+            <Stack spacing={2}>
+              <FormControl>
+                <TextField
+                  id="outlined-basic"
+                  required
+                  label="Name"
+                  name="Name"
+                  type="text"
+                  variant="outlined"
+                  sx={{ width: "100%" }}
+                />
+              </FormControl>
+              <FormControl>
+                <TextField
+                  id="outlined-basic"
+                  required
+                  label="Mail"
+                  name="email"
+                  aria-describedby="my-helper-text"
+                  variant="outlined"
+                  sx={{ width: "100%" }}
+                />
+                <FormHelperText id="my-helper-text">
+                  We'll never share your email.
+                </FormHelperText>
+                <ValidationError
+                  prefix="Email"
+                  field="email"
+                  errors={state.errors}
+                />
+              </FormControl>
+              <FormControl>
+                <TextField
+                  id="outlined-basic"
+                  type="tel"
+                  pattern="[0-9]{10}"
+                  required
+                  label="Phone"
+                  name="Phone"
+                  variant="outlined"
+                  sx={{ width: "100%" }}
+                />
+              </FormControl>
+              <FormControl>
+                <TextField
+                  label="Message"
+                  placeholder="Enter your message"
+                  id="message"
+                  name="message"
+                  multiline
+                  rows={4}
+                  sx={{ width: "100%" }}
+                />
+                <ValidationError
+                  prefix="Message"
+                  field="message"
+                  errors={state.errors}
+                />
+              </FormControl>
+
+              <Button
+                variant="contained"
+                sx={{
+                  maxWidth: "1rem",
+                  margin: "0 auto",
+                  padding: "0.5rem 4rem",
+                }}
+                disabled={state.submitting}
+                type="submit"
+              >
+                Submit
+              </Button>
+            </Stack>
+          </form>
         </Card>
       </Container>
     </>
