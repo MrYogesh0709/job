5ec071fbc67558903ca01cc44cb442d806cb1314
Mr.Yogesh <yogeshvanzara98@gmail.com>
2022-11-17 13:22:31 +0530

    about us

--- src/page/about/About.jsx
+++ src/page/about/About.jsx
@@ -132,6 +132,62 @@ const About = () => {
           </Grid>
         </Grid>
       </Container>
+      <Container sx={{ mt: "2rem", mb: "2rem" }}>
+        <Grid container spacing={2}>
+          <Grid item xs={12} sm={8}>
+            <Stack gap={2}>
+              <Typography
+                variant="h3"
+                textTransform="capitalize"
+                textAlign={"center"}
+              >
+                How We Work ?
+              </Typography>
+              <Divider />
+              <Typography variant="p" textTransform="capitalize">
+                At Advika Technology, we take care of all your problems. 100%
+                satisfaction is guaranteed, else we will refund your money, no
+                question asked. Now you can sit back and relax, we will provide
+                you best possible solutions in most reasonable prices. Once you
+                make bond with us, we will make sure you don't have to look
+                anywhere else for anything.
+                <br />
+                <br />
+                Send us mail on :
+                <Typography
+                  variant="p"
+                  component="a"
+                  href="mailto:advikatechnology.1207@gmail.com"
+                  sx={{ textDecoration: "none" }}
+                  target="_blank"
+                >
+                  advikatechnology.1207@gmail.com
+                </Typography>
+              </Typography>
+            </Stack>
+          </Grid>
+          <Grid
+            item
+            xs={12}
+            sm={4}
+            display="flex"
+            justifyContent={"center"}
+            alignItems={"center"}
+          >
+            <Card
+              sx={{ borderRadius: "24px 48px 24px 48px", boxShadow: "none" }}
+            >
+              <img
+                src={`${about_us}`}
+                alt="aobut_us"
+                loading="lazy"
+                width={"100%"}
+                size="cover"
+              />
+            </Card>
+          </Grid>
+        </Grid>
+      </Container>
     </>
   );
 };
